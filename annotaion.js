01aa489240e429319386a4fd36a44d96ff62445d
takagotch <dyaccb@gmail.com>
2018-02-15 07:27:37 +0900

    annotator.add(timestamp, message); annotator.update();

---
+++ annotaion.js
@@ -0,0 +1,7 @@
+var annotator = new Rickshaw.Graph.Annotate({
+
+});
+
+annotator.add();
+annotator.update();
+
--- i-hover_details.js
+++ i-hover_details.js
@@ -3,7 +3,9 @@ var hoverDetail = new Rickshaw.Graph.HoverDetail({
 });
 
 //
+var hoverDetail = new Rickshaw.Graph.HoverDetail({
 
+});
 
 
 
